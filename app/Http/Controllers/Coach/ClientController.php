6e3f6b885a59aa35ddb20c9f4277001e1956d028
YacineMARZOUK <yacinemarzou@gmail.com>
2025-04-13 14:42:03 +0100

    ajouter le filter par les objectifs

--- app/Http/Controllers/Coach/ClientController.php
+++ app/Http/Controllers/Coach/ClientController.php
@@ -15,31 +15,31 @@ class ClientController extends Controller
     }
     
     public function index(Request $request)
-{
-    $users = User::where('role', 'user')->get();
-
-    $query = UserGoal::whereHas('user', function ($q) {
-        $q->where('role', 'client');
-    })->with('user');
-
-    if ($request->filled('search')) {
-        $query->whereHas('user', function ($q) use ($request) {
-            $q->where('name', 'like', '%' . $request->search . '%');
-        });
+    {
+        $users = User::where('role', 'user')->get();
+    
+        $query = UserGoal::whereHas('user', function ($q) {
+            $q->where('role', 'client');
+        })->with('user');
+    
+        if ($request->filled('search')) {
+            $query->whereHas('user', function ($q) use ($request) {
+                $q->where('name', 'like', '%' . $request->search . '%');
+            });
+        }
+    
+        if ($request->filled('filters.user_id')) {
+            $query->where('user_id', $request->input('filters.user_id'));
+        }
+    
+        if ($request->filled('filters.objectif_principal')) {
+            $query->where('objectif_principal', $request->input('filters.objectif_principal'));
+        }
+    
+        $userGoals = $query->paginate(10);
+    
+        return view('coach.clients.index', compact('users', 'userGoals'));
     }
-
-    if ($request->filled('filters.user_id')) {
-        $query->where('user_id', $request->input('filters.user_id'));
-    }
-
-    if ($request->filled('filters.objectif_principal')) {
-        $query->where('objectif_principal', $request->input('filters.objectif_principal'));
-    }
-
-    $userGoals = $query->paginate(10);
-
-    return view('coach.clients.index', compact('users', 'userGoals'));
-}
-
+    
 
 }
